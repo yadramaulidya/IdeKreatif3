c0030d6aae6524118ff40a7285eddfbad5b2c6ab
yadramaulidya <yadra.maulidya39@smk.belajar.id>
2025-02-11 14:18:19 +0700

    menambahkan config

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "idekreatif";
+
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+
+die("database gagal terkoneksi: " . $conn->connect_error);
+}
+
+
+?>
