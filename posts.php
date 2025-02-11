9f9775244964166d8133c65f253bad1df9410696
yadramaulidya <yadra.maulidya39@smk.belajar.id>
2025-02-11 14:36:29 +0700

    menambahkan posts

--- posts.php
+++ posts.php
@@ -21,3 +21,20 @@ include '.includes/header.php';
                     <label for="formFile" class="form-label">Unggah Gambar</label>
                     <input class="form-control" type="file" name="image" accept="image/*" />
                  </div>
+                 <!-- Dropdown untuk memilih kategori -->
+                 <div class="mb-3">
+                    <label for="category_id" class="form-label">Kategori</label>
+                    <select class="form-select" name="category_id" required>
+                        <!-- Mengambil data kategori dari database u tuk mengisi opsi dropdown -->
+                         <option value="" selected disabled>Pilih salah satu</option>
+                         <?php
+                             $query = "SELECT * FROM categories"; // Query untuk mengambil data kategori
+                             $result = $conn->query($query); // Menjalankan query 
+                             if ($result->num_rows > 0) { // Jika terdapat data kategori
+                                while ($row = $result->fetch_assoc()) { // Iterasi setiap kategori
+                                    echo "<option value'" .$row["category_id"] . "'>" . $row["category_name"] . "</option>";
+                                }
+                            }
+                            ?>
+                            </select>
+                        </div>
