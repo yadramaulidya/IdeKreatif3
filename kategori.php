d1476fdc60fb70fd9c1ca343f0de4c7b1e04d498
yadramaulidya <yadra.maulidya39@smk.belajar.id>
2025-02-11 14:53:25 +0700

    menambahkan update

--- kategori.php
+++ kategori.php
@@ -81,6 +81,31 @@ include '.includes/toast_notification.php';
 </div>
 
           <!-- modal untuk update data kategori -->
+          <div id="editCategory_<?= $category['category_id']; ?>" class="modal fade" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Update Data Kategori</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+      <div class="modal-body">
+        <form action="proses_kategori.php" method="POST">
+          <!-- Input tersembunyi untuk menyimpan ID kategori -->
+          <input type="hidden" name="catID" value="<?= $category['category_id']; ?>">
+          <div class="form-group">
+            <label>Nama Kategori</label>
+            <!-- Input untuk nama kategori -->
+            <input type="text" value="<?= $category['category_name']; ?>" name="category_name" class="form-control">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+            <button type="submit" name="update" class="btn btn-warning">Update</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
           <?php endwhile; ?>
          </tbody>
