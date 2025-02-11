ac20dd365bc6d36117ba7f86b1a0d400e9a9eb83
yadramaulidya <yadra.maulidya39@smk.belajar.id>
2025-02-11 14:39:59 +0700

    menambahkan init_session

---
+++ .includes/init_session.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+$userId= $_SESSION["user_id"];
+$name = $_SESSION["name"];
+$role = $_SESSION["role"];
+// Ambil notifikasi jika ada, kemudian hapus dari sesi
+$notification = $_SESSION['notification'] ?? null;
+if ($notification) {
+    unset($_SESSION['notification']);
+}
+if (empty($_SESSION["username"]) || empty($_SESSION["role"])) {
+    $_SESSION['notification'] = [
+        'type' => 'danger',
+        'message' => 'Silahkan Login Terlebih Dahulu!'
+    ];
+    header('Location: ./auth/login.php');
+    exit();
+}
